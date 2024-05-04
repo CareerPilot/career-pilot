122cdda5eacadd2ad490395c1358e5f1c8cd0028
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2024-05-03 17:09:01 -0700

    Add doc_to_text function (#9)
    This function will accept a path to a DOC or DOCX file and return a
    string of all text in the file.

--- helpers.py
+++ helpers.py
@@ -1,4 +1,19 @@
+from docx_parser import DocumentParser
 from pypdf import PdfReader
+
+def doc_to_text(doc_file_path: str) -> str:
+    def item_to_text(itemType, item) -> str:
+        if itemType == 'multipart':
+            text = '\n'.join(s for s in item if type(s) == str)
+        elif itemType == 'paragraph':
+            text = item['text']
+        elif itemType == 'table':
+            g = (s for s, _ in it.groupby(it.chain.from_iterable(item['data'])))
+            text = '\n'.join(filter(None, g))
+        return text
+    doc = DocumentParser(doc_file_path)
+    text = '\n'.join(item_to_text(*t) for t in doc.parse())
+    return text
 
 def pdf_to_text(pdf_file_path: str) -> str:
     reader = PdfReader(pdf_file_path)
