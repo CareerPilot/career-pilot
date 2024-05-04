5ddb1b4122d5b31a0f646b415a404b67b7faa227
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2024-05-04 10:04:46 -0700

    Add support for doc, docx, and txt files (#11)
    * Add support for doc, docx, and txt files
    
    * fix docx upload bug
    
    ---------
    
    Co-authored-by: Raghav Goswami <raghav.goswami576@gmail.com>

--- Home.py
+++ Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from helpers import pdf_to_text
+from helpers import doc_to_text, pdf_to_text
+from io import StringIO
 
 # Main title
 st.title("CareerPilot")
@@ -9,7 +10,7 @@ st.caption("Where your career takes flight")
 with st.container():
     st.markdown("<h3>1. Paste or Upload Resume</h3>", unsafe_allow_html=True)
     resume_text = st.text_area("Paste resume:")
-    pdf_file = st.file_uploader("Upload resume as PDF:", type="pdf")  # No width specified
+    uploaded_file = st.file_uploader("Upload resume as DOC, DOCX, PDF, or TXT:", type=["doc", "docx", "pdf", "txt"])  # No width specified
 
 st.markdown("---")  # Adds a horizontal line for better separation
 
@@ -22,13 +23,19 @@ st.markdown("---")  # Adds a horizontal line for better separation
 # Button to generate the coaching report
 st.markdown("<h3>3. Get Coaching Report</h3>", unsafe_allow_html=True)
 
-if (resume_text or pdf_file) and job_description_text:
+if (resume_text or uploaded_file) and job_description_text:
     if st.button("Generate Coaching Report"):
         # Set session state only when the button is clicked
         if resume_text:
             st.session_state.resume_text = resume_text
-        elif pdf_file is not None:
-            st.session_state.resume_text = pdf_to_text(pdf_file)
+        elif uploaded_file is not None:
+            name = uploaded_file.name.lower()
+            if name.endswith('pdf'):
+                st.session_state.resume_text = pdf_to_text(uploaded_file)
+            elif name.endswith('doc') or name.endswith('docx'):
+                st.session_state.resume_text = doc_to_text(uploaded_file)
+            else:
+                st.session_state.resume_text = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
 
         st.session_state.job_description_text = job_description_text
 
--- helpers.py
+++ helpers.py
@@ -1,19 +1,10 @@
-from docx_parser import DocumentParser
 from pypdf import PdfReader
+from docx import Document
 
-def doc_to_text(doc_file_path: str) -> str:
-    def item_to_text(itemType, item) -> str:
-        if itemType == 'multipart':
-            text = '\n'.join(s for s in item if type(s) == str)
-        elif itemType == 'paragraph':
-            text = item['text']
-        elif itemType == 'table':
-            g = (s for s, _ in it.groupby(it.chain.from_iterable(item['data'])))
-            text = '\n'.join(filter(None, g))
-        return text
-    doc = DocumentParser(doc_file_path)
-    text = '\n'.join(item_to_text(*t) for t in doc.parse())
-    return text
+def doc_to_text(doc_file_path):
+    doc = Document(doc_file_path)
+    return '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+
 
 def pdf_to_text(pdf_file_path: str) -> str:
     reader = PdfReader(pdf_file_path)
