53824ffb77cefc98286cdc46d4ac05f1af391d28
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2024-05-04 21:43:01 -0700

    Replace docx with python-docx (#13)
    The docx package is an older and no longer supported version of the
    python-docx package.  Also, it does not run on my system.

--- helpers.py
+++ helpers.py
@@ -1,9 +1,21 @@
 from pypdf import PdfReader
-from docx import Document
+import docx
+import itertools as it
 
-def doc_to_text(doc_file_path):
-    doc = Document(doc_file_path)
-    return '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+def doc_to_text(doc_file_path: str) -> str:
+    def item_to_text(tp) -> str:
+        if type(tp) == docx.text.paragraph.Paragraph:
+            text = tp.text
+        elif type(tp) == docx.table.Table:
+            g = (s for s, _ in it.groupby(c.text for r in tp.rows for c in r.cells))
+            text = '\n'.join(filter(None, g))
+        else:
+            raise ValueError(f'unexpected {type(tp)} object in document')
+        return text
+    doc = docx.Document(doc_file_path)
+    g = (tp for s in doc.sections for tp in s.iter_inner_content())
+    text = '\n'.join(item_to_text(tp) for tp in g)
+    return text
 
 
 def pdf_to_text(pdf_file_path: str) -> str:
