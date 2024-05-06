de1cbfe4c2f8a3c87536ca4a30d89c970ca2b4e9
Raghav Goswami <raghav.goswami576@gmail.com>
2024-05-06 00:55:41 -0400

    generate coaching report (#17)

--- pages/Coaching_Report.py
+++ pages/Coaching_Report.py
@@ -1,9 +1,11 @@
 import streamlit as st
+from langchain_community.llms import Replicate
+
 st.title("CareerPilot")
 st.caption("Where your career takes flight")
 st.divider()
 st.markdown("<h3>Coaching Report</h3>", unsafe_allow_html=True)
-st.caption("Get insights on how well your resume matches the job description")
+st.caption("Get insights on how well your resume matches the job description.")
 
 # Display an error if there is one
 if 'error' in st.session_state:
@@ -13,12 +15,24 @@ elif not st.session_state.get('resume_text') or not st.session_state.get('job_de
     if st.button("Go Home", type="primary"):
       st.switch_page("Home.py")
 else:
-    # Here you would include the logic to process the resume and job description
-    # and generate the coaching report.
-    # For example, you could display the result like this:
-    # report = generate_coaching_report(st.session_state.resume_text, st.session_state.job_description_text)
-    # st.write(report)
-    st.write(f"This is a placeholder for coaching report")
-    st.write(f"{st.session_state.resume_text=}, {st.session_state.job_description_text=}")
-    if st.button("Talk to AI Resume Coach", type="primary"):
-      st.switch_page("pages/AI_Resume_Coach.py")
+    # Initialize the LLM
+    replicate_llm = Replicate(
+        model="meta/meta-llama-3-8b-instruct",
+        model_kwargs={"temperature": 0.75, "max_length": 500, "top_p": 1}
+    )
+
+    prompt = f"""Please analyze the following resume and job description and generate a comprehensive coaching report on how well the resume matches the job description.
+    Start by giving a matching score out of 10.
+    Here is the resume: {st.session_state.resume_text}.
+    And here is the job description: {st.session_state.job_description_text}"""
+
+    st.write(prompt) # TODO: Remove this after testing
+    # Generate the coaching report
+    try:
+        response = replicate_llm.invoke(prompt)  # Directly call the model with the prompt
+        st.session_state.coaching_report = response  # Save the response in session state
+        st.write(response)  # Display it
+    except Exception as e:
+        st.error(f"Failed to generate a coaching report: {str(e)}")
+
+
